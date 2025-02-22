2c3d65b4d29da608ecb4fac18c0b330214668ebe
Nmihal1 <nmihal28@gmail.com>
2025-02-22 11:24:29 +0200

    MC 22.2.2025 - Adagio for strings and checks

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -5,7 +5,16 @@ public class Program {
 
     static void Main(string[] args) {
         //string[] samples = Dump.MemoryGarbage(20, 100);
-        string[] samples = Dump.MemoryGarbage(26);
+        //string[] samples = Dump.MemoryGarbage(26);
+
+        string[] samples = { "cduquptfjc", "mvhhtipiux", "vxartnpdye", "kqrglkzwys", "kyupkxlnkx" };
+
+        const string KEY = "kyupkxlnkx";
+
+        /*string[] samples = { "cduquptfjc", "mvhhtipiux", "vxartnpdye", "kqrglkzwys", "kyupkxlnkx", "qgqueygrqt", "osnkjqyyss", "wsdsoekubn", "mrpxbdbokg", "vlhbbgygpt", "kvwfwqxdfb",
+        "jksgdtqgqd", "ovokeqmhlf", "rdmkhzgezd", "thcdwljvnn", "ulxsahdqew", "rjenalvjmt", "juhaxggzfv", "ddfusqhxfu", "nawviotkkr", "arzudqslfy", "fueztqovwm", "zuawxdnsbj", "evmxdbfqhy",
+        "ejebpoifkm", "mjxkyxhljw"};*/
+
 
         Console.ForegroundColor = ConsoleColor.Green;
         for (int i = 0; i < samples.Length; i++) {
@@ -20,8 +29,8 @@ public class Program {
         stopwatch.Start();
         for (int i = 0; i < TIMES; i++) {
             string key = samples[i % samples.Length];
-            //int result = Test.TestIf("e");
-            int result = Test.TestIf(key);
+            int result = Test.TestIfIf(KEY);
+            //int result = Test.TestIf(key);
         }
         stopwatch.Stop();
         Console.WriteLine($"If end - Elapsed: {stopwatch.Elapsed.TotalSeconds}");
@@ -31,8 +40,8 @@ public class Program {
         stopwatch.Start();
         for (int i = 0; i < TIMES; i++) {
             string key = samples[i % samples.Length];
-            int result = Test.TestSwitch(key);
-            //int result = Test.TestSwitch("e");
+            //int result = Test.TestSwitch(key);
+            int result = Test.TestSwitchSwitch(KEY);
         }
         stopwatch.Stop();
         Console.WriteLine($"Switch end - Elapsed: {stopwatch.Elapsed.TotalSeconds}");
@@ -42,7 +51,8 @@ public class Program {
         stopwatch.Start();
         for (int i = 0; i < TIMES; i++) {
             string key = samples[i % samples.Length];
-            int result = Test.TestDictionary(key);
+            //int result = Test.TestDictionary(key);
+            int result = Test.TestDictionary(KEY);
         }
         stopwatch.Stop();
         Console.WriteLine($"Safe dic end - Elapsed: {stopwatch.Elapsed.TotalSeconds}");
@@ -52,7 +62,8 @@ public class Program {
         stopwatch.Start();
         for (int i = 0; i < TIMES; i++) {
             string key = samples[i % samples.Length];
-            int result = Test.UnsafeTestDictionary(key);
+            //int result = Test.UnsafeTestDictionary(key);
+            int result = Test.UnsafeTestDictionary(KEY);
         }
         stopwatch.Stop();
         Console.WriteLine($"Unsafe dic end - Elapsed: {stopwatch.Elapsed.TotalSeconds}");
--- DataStructures/Test.cs
+++ DataStructures/Test.cs
@@ -7,6 +7,37 @@
         }
     }
 
+    public static int TestIfIf(string _key) {
+        if (_key == "cduquptfjc") { return 1; }
+        if (_key == "mvhhtipiux") { return 2; }
+        if (_key == "vxartnpdye") { return 3; }
+        if (_key == "kqrglkzwys") { return 4; }
+        if (_key == "kyupkxlnkx") { return 5; }
+        if (_key == "qgqueygrqt") { return 6; }
+        if (_key == "osnkjqyyss") { return 7; }
+        if (_key == "wsdsoekubn") { return 8; }
+        if (_key == "mrpxbdbokg") { return 9; }
+        if (_key == "vlhbbgygpt") { return 10; }
+        if (_key == "kvwfwqxdfb") { return 11; }
+        if (_key == "jksgdtqgqd") { return 12; }
+        if (_key == "ovokeqmhlf") { return 13; }
+        if (_key == "rdmkhzgezd") { return 14; }
+        if (_key == "thcdwljvnn") { return 15; }
+        if (_key == "ulxsahdqew") { return 16; }
+        if (_key == "rjenalvjmt") { return 17; }
+        if (_key == "juhaxggzfv") { return 18; }
+        if (_key == "ddfusqhxfu") { return 19; }
+        if (_key == "nawviotkkr") { return 20; }
+        if (_key == "arzudqslfy") { return 21; }
+        if (_key == "fueztqovwm") { return 22; }
+        if (_key == "zuawxdnsbj") { return 23; }
+        if (_key == "evmxdbfqhy") { return 24; }
+        if (_key == "ejebpoifkm") { return 25; }
+        if (_key == "mjxkyxhljw") { return 26; }
+
+        return -1;
+    }
+
     public static int TestIf(string _key) {
         if (_key == "a") { return 1; }
         if (_key == "b") { return 2; }
@@ -34,6 +65,39 @@
         if (_key == "x") { return 24; }
         if (_key == "y") { return 25; }
         if (_key == "z") { return 26; }
+
+        return -1;
+    }
+
+    public static int TestSwitchSwitch(string _key) {
+        switch (_key) {
+            case "cduquptfjc": { return 1; }
+            case "mvhhtipiux": { return 2; }
+            case "vxartnpdye": { return 3; }
+            case "kqrglkzwys": { return 4; }
+            case "kyupkxlnkx": { return 5; }
+            case "qgqueygrqt": { return 6; }
+            case "osnkjqyyss": { return 7; }
+            case "wsdsoekubn": { return 8; }
+            case "mrpxbdbokg": { return 9; }
+            case "vlhbbgygpt": { return 10; }
+            case "kvwfwqxdfb": { return 11; }
+            case "jksgdtqgqd": { return 12; }
+            case "ovokeqmhlf": { return 13; }
+            case "rdmkhzgezd": { return 14; }
+            case "thcdwljvnn": { return 15; }
+            case "ulxsahdqew": { return 16; }
+            case "rjenalvjmt": { return 17; }
+            case "juhaxggzfv": { return 18; }
+            case "ddfusqhxfu": { return 19; }
+            case "nawviotkkr": { return 20; }
+            case "arzudqslfy": { return 21; }
+            case "fueztqovwm": { return 22; }
+            case "zuawxdnsbj": { return 23; }
+            case "evmxdbfqhy": { return 24; }
+            case "ejebpoifkm": { return 25; }
+            case "mjxkyxhljw": { return 26; }
+        }
 
         return -1;
     }
